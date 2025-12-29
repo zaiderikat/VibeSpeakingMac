a9f10ba4f870ce55c57cb5235c0d859d67f2d2f0
Zaid Erikat <zaidere@gmail.com>
2025-12-28 18:04:15 -0800

    Initial Commit

---
+++ VoiceVibing/VoiceVibingApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceVibingApp.swift
+//  VoiceVibing
+//
+//  Created by Zaid Erekat on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceVibingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoiceVibingTests/VoiceVibingTests.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceVibingTests.swift
+//  VoiceVibingTests
+//
+//  Created by Zaid Erekat on 12/28/25.
+//
+
+import Testing
+@testable import VoiceVibing
+
+struct VoiceVibingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
